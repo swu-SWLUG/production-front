76be98ddbd8567640a31d01fedf8348df8d0dc2c
mnzy412 <mnzy412@gmail.com>
2025-03-05 03:53:00 +0900

    [mnzy] 네이버 검색 엔진 최적화

--- src/App.js
+++ src/App.js
@@ -6,26 +6,35 @@ import Footer from './components/Footer';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import ScrollToTop from './components/ScrollToTop';
+import { Helmet } from 'react-helmet-async';
 
 import "./styles/common.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App flex flex-col min-h-screen">
-        <Header />
-        <main className="min-h-screen">
-          <div className="container mx-auto text-center">
-            <Suspense fallback={<div>Loading...</div>}>
-              <RouterProvider router={root}>
-                <ScrollToTop /> 
-              </RouterProvider>
-            </Suspense>
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </Provider>
+      <Provider store={store}>
+        <div className="App flex flex-col min-h-screen">
+          <Helmet>
+            <title>SWLUG</title>
+            <meta name="description" content="서울여자대학교 정보보호학부 소학회 SWLUG" />
+            <meta property="og:title" content="SWLUG" />
+            <meta property="og:description" content="서울여자대학교 정보보호학부 소학회 SWLUG" />
+            <meta property="og:url" content="https://www.swlug.com" />
+            {/* 네이버 검색엔진 최적화를 위한 추가 메타 태그 */}
+          </Helmet>
+          <Header />
+          <main className="min-h-screen">
+            <div className="container mx-auto text-center">
+              <Suspense fallback={<div>Loading...</div>}>
+                <RouterProvider router={root}>
+                  <ScrollToTop />
+                </RouterProvider>
+              </Suspense>
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </Provider>
   );
 }
 
